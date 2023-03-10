4d65f570d759d920715c7200d2d5226d46b931fa
akshay2903 <akshaypal2903@gmail.com>
2023-03-11 00:50:50 +0530

    second components commit

--- App.js
+++ App.js
@@ -1,45 +1,41 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
 
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
-
+const App = () => {
   const expenses = [
     {
       id: 'e1',
-      title: 'Watching movies',
-      amount: 200,
-      date: new Date(2022, 3, 19),
-      place: 'Meerut'
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
     },
-    {
-      id: 'e2',
-      title: 'Buying groceries',
-      amount: 1200,
-      date: new Date(2022, 2, 29),
-      place: 'Noida'
-    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'Buying Clothes',
-      amount: 2000,
-      date: new Date(2022, 4, 11),
-      place: 'Gurugram'
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'Eating Pizza',
-      amount: 700,
-      date: new Date(2022, 5, 12),
-      place: 'Delhi'
-    }
-  ]
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h1>Learn React</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} place={expenses[0].place}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} place={expenses[1].place}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} place={expenses[2].place}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} place={expenses[3].place}/>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
