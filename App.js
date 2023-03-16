8c47e1260dee65abfa86e1b3d6ffcfadfa16ed26
akshay2903 <akshaypal2903@gmail.com>
2023-03-16 21:26:21 +0530

    Making the expense form input visible on screen

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -24,18 +24,19 @@ const App = () => {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ];
+  ]);
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
